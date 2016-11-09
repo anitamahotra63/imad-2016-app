f81931088eb423658247094bfec1571d615c4cec
anitamahotra63 <anitamahotra63@gmail.com>
2016-11-09 16:10:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -178,7 +178,15 @@ app.get('/',function(req,res){
    res.sendFile(path.join(__dirname,'ui','index.html')); 
 });
 
-
+ var names=[];
+app.get('/submit-name/:name',function(req,res){
+    
+    var name=req.params.name;
+    
+    names.push(name);
+    
+    res.send(JSON.stringify(names));
+});
 
 app.get('/:ArticleName',function(req,res){
     var articleName=req.params.ArticleName;
