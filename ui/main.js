a3f0774c663b90e06a19e1fb2d02ea6f9f0cb7f6
anitamahotra63 <anitamahotra63@gmail.com>
2016-11-09 16:30:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,14 +2,14 @@
 var nameInput=document.getElementById('name');
 var nameValue=nameInput.value;
 var submit=document.getElementById('sbmt_btn');
-submit.onclick=function(){
+submit.onclick = function(){
    
     var names=["anita","sunita","anuj"];
     var list='';
     for(var i=0;i<names.length;i++){
         list +='<li>'+names[i].toString()+'</li>';
     }
-    
+    list='hello';
     var ul=document.getElementById('list');
     ul.innerHTML=list;
     
