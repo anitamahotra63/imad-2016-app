7d09ab21fc77c1933dc71feba1c0865465cca186
anitamahotra63 <anitamahotra63@gmail.com>
2016-11-09 15:10:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,5 +3,26 @@ var nameInput=document.getElementById('name');
 var nameValue=nameInput.value;
 var submit=document.getElementById('sbmt_btn');
 submit.onclick=function(req,res){
+    var request = new XMLHttprequest();
     
+    request.onreadystatechange=function(){
+        if(request.readyState ===XMLHttpRequest.DONE){
+            //take some action
+            if(request.status===200){
+                var names=request.responseText;
+                names=JSON.parse(names);
+                var list='';
+                for(var i=0;i<names.length;i++){
+                    list+='<li>'+names[i]+'</li>';
+                }
+                var ul=document.getElementById('namelist');
+                ul.innerHTML=list;
+            }
+        }
+        
+        //not done yet.
+    };
+    
+    request.open('GET','http://anitamahotra63.imad.hasura-app.io/submit-name?name='+nameValue,true);
+    request.send(null);
 };
