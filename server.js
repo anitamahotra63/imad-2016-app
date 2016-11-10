4c3e9b86436cc75de95a4d2ae6190238cc8bb173
anitamahotra63 <anitamahotra63@gmail.com>
2016-11-10 18:37:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,6 +95,31 @@ app.post('/create-user',function(req,res){
    });
 });
 
+app.post('/login',function(req,res){
+   
+   var username=req.body.username;
+   var password=req.body.password;
+   
+   pool.query('SELECT * FROM "user" WHERE username = $1',[username],function(err,result){
+       if(err){
+           res.status(500).send(err.toString());
+       }else{
+           if(result.rows.length ===0){
+               res.status(403).send("username/password is invalid");
+           }else{
+               var dbString = result.rows[0].password;
+               var salt = dbString.split('$')[2];
+               var hashedPassword = hash(password,salt);
+               if(hashPassword ===dbString){
+                   res.send('Credentials are correct');
+               }else{
+                   res.send(403).send("Invalid username/password");
+               }
+           }
+       }
+   });
+});
+
  var names=[];
 app.get('/submit-name',function(req,res){
     
