2dfd7476c51ced5530f60d4b4d38c6f6bc7f2161
anitamahotra63 <anitamahotra63@gmail.com>
2016-11-10 20:49:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-//Submit Name
+//Submit username and password to login
 
 var submit=document.getElementById('sbmt_btn');
 submit.onclick = function(){
@@ -30,9 +30,9 @@ submit.onclick = function(){
    };
    var nameInput=document.getElementById('name');
     var nameValue=nameInput.value;
-   request.open('GET','http://anitamahotra63.imad.hasura-app.io/submit-name?name='+nameValue,true);
+   request.open('POST','http://anitamahotra63.imad.hasura-app.io/submit-name?name='+nameValue,true);
    
-   request.send(null);
+   request.send();
     
     
 };
