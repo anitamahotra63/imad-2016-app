5fb6ba92520489f2b7bc24200622676c1fc3062f
anitamahotra63 <anitamahotra63@gmail.com>
2016-11-11 14:37:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,13 +65,16 @@ app.get('/hash/:input',function(req,res){
 
 app.post('/create-user',function(req,res){
    
+   var name = req.body.name;
    var username = req.body.username;
    var password = req.body.password;
+   var idea = req.body.idea;
+   var age = req.body.age;
    
    var salt = crypto.randomBytes(128).toString('hex');
    var dbString = hash(password,salt); 
    
-   pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)', [username,dbString],function(err,result){
+   pool.query('INSERT INTO "user" (name,username,password,idea,age) VALUES ($1,$2,$3,$4,$5)', [name,username,dbString,idea,age],function(err,result){
        if(err){
            res.status(500).send(err.toString());
        }else{
