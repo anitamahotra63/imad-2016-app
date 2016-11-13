80dd41ff75d8c421d4274d4d1a44cd40e0c3d57f
anitamahotra63 <anitamahotra63@gmail.com>
2016-11-13 16:32:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.post('/login',function(req,res){
                 if(hashedPassword === dbString){
                     
                     //set the session
-                    req.session.auth = {userid : result.rows[0].id};
+                    req.session.auth = {userId : result.rows[0].id};
                     
                     res.send("credentials are correct!!");
                 }
@@ -91,11 +91,17 @@ app.post('/login',function(req,res){
 
 app.get('/check-login',function(req,res){
    
-    if (req.session && req.session.auth && req.session.auth.userId) {
-       
-       res.send("you are logged in!!")
+    if (req.session  &&  req.session.auth  &&  req.session.auth.userId) {
+       // Load the user object
+       pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
+           if (err) {
+              res.status(500).send(err.toString());
+           } else {
+              res.send(result.rows[0].username);    
+           }
+       });
    } else {
-       res.status(400).send("You are not logged in");
+       res.status(400).send('You are not logged in');
    }
 });
 
