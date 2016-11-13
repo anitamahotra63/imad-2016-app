151717914e5733f5633d0fce48b392b4e0b76139
anitamahotra63 <anitamahotra63@gmail.com>
2016-11-13 13:16:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ app.get('/check-login',function(req,res){
            }
        });
    } else {
-       res.status(400).send('You are not logged in');
+       res.status(400).send("You are not logged in");
    }
 });
 
