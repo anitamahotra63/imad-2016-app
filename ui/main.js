1b997ed9561303888cd417094cf07e7baafd50d3
anitamahotra63 <anitamahotra63@gmail.com>
2016-11-13 12:09:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -118,8 +118,6 @@ function loadLogin () {
                 }else{
                     loadLoggedInUser(this.responseText);
                 }
-            } else {
-                loadLoginForm();
             }
         }
     };
