919ae48da6aa391645a40f8b5da65b7490dff837
anitamahotra63 <anitamahotra63@gmail.com>
2016-11-23 00:39:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -53,6 +53,8 @@ app.post('/create-user',function(req,res){
        if(err){
            res.status(500).send(err.toString());
        }else{
+           
+           req.session.auth = {userId : result.rows[0].id};
            res.send('User successfully created '+username);
        }
    });
